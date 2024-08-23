fd6f29c4a487056ea6d93a90e4cd071834de9685
9494Guzel <guzel.galiulina.94@inbox.ru>
2024-08-24 00:13:26 +0300

    Add files via upload

---
+++ module5.1.py
@@ -0,0 +1,14 @@
+class House:
+    def __init__(self, name, namberOfFloors):
+       self.name= name
+       self.namberOfFloors= namberOfFloors
+
+    def go_to(new_floor):
+        с= 1
+        print(c< new_floor)
+        if new_floor > self.namberOfFloors or new_floor < self.namberOfFloors:
+            print('такого этажа не существует')
+
+ h1=House('жк Горький')
+ h2= House(5)
+ print((h1.name,h2.namberOfFloors ))
